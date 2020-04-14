90cd313dfe87f1eed5e35721d5aacd9be883049f
Chase Swanstrom <chaseswanstrom@Chases-MacBook-Pro.local>
2020-04-13 23:15:45 -0700

    initial commit

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from datetime import date, datetime
 import constants
 import csv
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def california():
@@ -30,8 +32,31 @@ def california():
         dates.append(date)
         max_deaths.append(deaths)
     
+    print(hourly_dict)
     return render_template('california.html', labels = dates, values = max_deaths,
      updated_time = hourly_data[-1].split(',')[1], updated_day = hourly_data[-1].split(',')[0])
-                
+
+
+@app.route('/timer', methods =['POST', 'GET'])
+def timer():
+    if request.method == 'POST':
+        try:
+            start = request.form['start']
+            day = start[3:5]
+            month = start[0:2]
+            year = start[6:]
+            start = date(int(year), int(month), int(day))
+            today = date.today()
+            if start > today:
+                return redirect(url_for('california'))
+            delta = today - start
+            return render_template('days.html', days = delta.days)
+        except ValueError:
+            return redirect(url_for('california'))
+
+    return render_template('days.html')
+
+
+
 if __name__ == "__main__":
 	app.run()
